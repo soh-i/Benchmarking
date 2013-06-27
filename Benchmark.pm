cc906f2031a321f83796aa4327cc50afe03b19cf
soh-i <soh.ishiguro3914@gmail.com>
2013-06-27 13:59:20 +0900

    combined two methods to one and added flag option

--- Benchmark.pm
+++ Benchmark.pm
@@ -6,6 +6,52 @@ use List::Compare;
 use Carp;
 use Data::Dumper;
 
+sub get_measure {
+    my %args = (
+                candidate => undef,
+                answerset => undef,
+                flag      => undef,
+                @_,
+               );
+    
+    # type checking 
+    if (ref $args{candidate} ne 'ARRAY' && ref $args{answerset} ne 'ARRAY') {
+        carp "Error: given data are not ARRAY";
+    }
+    # flag checking
+    if (!$args{flag} eq 'precision' || !$args{flag} eq 'recall' || !$args{flag} eq 'all') {
+        carp "Error: given a flag is not accepted";
+    }
+    # NA checking
+    my $ans_data_count       = scalar @{ $args{answerset} };
+    my $candidate_data_count = scalar @{ $args{candidate} };
+        
+    if ($ans_data_count != 0 && $candidate_data_count != 0) {
+        my $lc = List::Compare->new($args{candidate}, $args{answerset});
+        my $intersection = $lc->get_intersection();
+        
+        if ($args{flag} eq 'precision') {
+            my $recall = $intersection/$candidate_data_count;
+            return sprintf "%2.4f", $recall;
+        }
+        elsif ($args{flag} eq 'recall') {
+            my $precision = $intersection/$ans_data_count;
+            return sprintf "%2.4f", $precision;
+        }
+        elsif ($args{flag} eq 'all') {
+            my $r = $intersection/$candidate_data_count;
+            my $p = $intersection/$ans_data_count;
+            return "$r, $p";
+        } else {
+            return;
+        }
+    }
+}
+
+1;
+
+__END__
+
 sub calc_recall {
     my %args = (
                 candidate => undef,
@@ -56,5 +102,6 @@ sub calc_precision {
         return sprintf "%2.4f", ($intersection / $candidate_data_count);
     }
 }
+
 
 1;
