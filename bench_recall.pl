5c5e5acc837f3c6a2b1dd040866d4d61eec3cb8e
soh-i <soh.ishiguro3914@gmail.com>
2013-06-29 13:07:47 +0900

    Add constant of suffix of the input files, and do not use global variable

--- bench_recall.pl
+++ bench_recall.pl
@@ -9,6 +9,7 @@ use Cwd;
 use File::Basename qw/fileparse basename/;
 use List::Compare;
 use Getopt::Long;
+use Readonly;
 use Data::Dumper;
 
 # My modules
@@ -34,11 +35,15 @@ if (!$opts{answer_dir} || !$opts{predicted_dir}) {
     die _usage();
 }
 
+# Constatnt variables
+Readonly my $ANSDATA_SUFFIX        => '.txt'; # eg. hg19.txt
+Readonly my $PREDICTED_DATA_SUFFIX => '.csv'; # eg. excel.csv
+
 # Collect answer data set from given dir
 my $adh = IO::Dir->new($opts{answer_dir}) or die $!;
 my @ans_files = ();
 while (my $ans_file = $adh->read()){
-    next unless $ans_file =~ m/\.txt/;
+    next unless $ans_file =~ m/$ANSDATA_SUFFIX$/;
     my $abs_ans_path = Cwd::abs_path($opts{answer_dir})."/$ans_file";
     push @ans_files, $abs_ans_path;
 }
@@ -57,7 +62,7 @@ for my $ansfile (@ans_files) {
     
     # collection of predicted data
     while (my $pred_file = $pdh->read()){
-        next unless $pred_file =~ m/\.csv$/;
+        next unless $pred_file =~ m/$PREDICTED_DATA_SUFFIX$/;
         my $abs_pred_path = Cwd::abs_path($opts{predicted_dir})."/$pred_file";
         my $collected_predict = collect_data(file => $abs_pred_path, sep => 'comma');
         
@@ -81,23 +86,22 @@ sub collect_data {
                 @_,
                );
     
-    my $bfh = IO::File->new( $args{file} ) or croak "Cant open file:$!";
+    my $bfh = IO::File->new($args{file}) or croak "Cant open file:$!";
     my $collected = [];
     while (my $data_entory = $bfh->getline) {
-        next if $. == 1;
+        next if __LINE__ == 1;
 
         # Split properly
-        my ($chr, $pos);
         if ($args{sep} eq 'tab') {
-            ($chr, $pos)  = (split /\t/, $data_entory)[0,1];
+            my ($chr, $pos)  = (split /\t/, $data_entory)[0,1];
             $chr =~ s/^chr//g if $chr =~ m/^chr/; # Remove 'chr' prefix
             $pos =~ s/\,//g   if $pos =~ m/\,/;   # Remove comma. e.g. 123,456
             push $collected, "$chr:$pos";
         }
         elsif ($args{sep} eq 'comma') {
-            ($chr, $pos)  = (split /\,/, $data_entory)[0,1];
+            my ($chr, $pos)  = (split /\,/, $data_entory)[0,1];
             $chr =~ s/^chr//g if $chr =~ /^chr/;
-            $pos =~ s/\,//g if $pos =~ m/\,/;
+            $pos =~ s/\,//g   if $pos =~ m/\,/;
             push $collected, "$chr:$pos";
         }
         else {
@@ -120,6 +124,11 @@ Usage:
 
 Options:
     --help Show help message
+
+*** Input data ***
+[0] chr1 [1] 1338121
+[0] YHet, [1] 12,009
+[0] 1, [1] 9318212
 
 EOF
 }
