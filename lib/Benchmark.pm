a58eee1960ccd7dc9e773029b0a4a0692ad62b76
soh-i <soh.ishiguro3914@gmail.com>
2013-06-29 13:16:10 +0900

    fixed namespace

--- lib/Benchmark.pm
+++ lib/Benchmark.pm
@@ -1,68 +1,72 @@
-package Benchmark;
-
-use strict;
-use warnings;
-use List::Compare;
-use Carp;
-use Data::Dumper;
-
-sub get_measure {
-    my %args = (
-                predicted => undef,
-                answerset => undef,
-                flag      => undef,
-                @_,
-               );
+package Benchmark {
     
-    # type checking 
-    if (ref $args{predicted} ne 'ARRAY' && ref $args{answerset} ne 'ARRAY' && ref $args{flag} ne 'HASH') {
-        croak "Error: given data are not ARRAY";
-    }
+    use strict;
+    use warnings;
+    use List::Compare;
+    use Carp;
+    use Data::Dumper;
     
-    # flag checking
-    if (!defined $args{flag}) {
-        croak "Error: Setting 'flag' param to get_measure() is required";
-    }
-    elsif (!defined $args{predicted}) {
-        croak "Error: Setting 'predicted' param to get_measure() is required";
-    }
-    elsif (!defined $args{answerset}) {
-        croak "Error: Setting 'answerset' param to get_measure() is required";
-    }
-    
-    # NA checking
-    my $ans_data_count       = scalar @{ $args{answerset} };
-    my $predicted_data_count = scalar @{ $args{predicted} };
+    sub get_measure {
+        my %args = (
+                    predicted => undef,
+                    answerset => undef,
+                    flag      => undef,
+                    @_,
+                   );
         
-    if ($ans_data_count != 0 && $predicted_data_count != 0) {
-        my $lc = List::Compare->new($args{predicted}, $args{answerset});
-        my $intersection = $lc->get_intersection();
+        # type checking 
+        if (ref $args{predicted} ne 'ARRAY' && ref $args{answerset} ne 'ARRAY' && ref $args{flag} ne 'HASH') {
+            croak "Error: given data are not ARRAY";
+        }
         
-        if ($args{flag} eq 'recall') {
-            my $recall = $intersection/$ans_data_count;
-            return _formated($recall);
+        # flag checking
+        if (!defined $args{flag}) {
+            croak "Error: Setting 'flag' param to get_measure() is required";
         }
-        elsif ($args{flag} eq 'precision') {
-            my $precision = $intersection/$predicted_data_count;
-            return _formated($precision);
+        elsif (!defined $args{predicted}) {
+            croak "Error: Setting 'predicted' param to get_measure() is required";
         }
-        elsif ($args{flag} eq 'all') {
-            my $recall    = _formated($intersection/$ans_data_count);
-            my $precision = _formated($intersection/$predicted_data_count);
-            my $res = "$recall\t$precision";
-            return $res;
+        elsif (!defined $args{answerset}) {
+            croak "Error: Setting 'answerset' param to get_measure() is required";
+        }
+        
+        # NA checking
+        my $ans_data_count       = scalar @{ $args{answerset} };
+        my $predicted_data_count = scalar @{ $args{predicted} };
+        
+        if ($ans_data_count != 0 && $predicted_data_count != 0) {
+            my $lc = List::Compare->new($args{predicted}, $args{answerset});
+            my $intersection = $lc->get_intersection();
+            
+            if ($args{flag} eq 'recall') {
+                my $recall = $intersection/$ans_data_count;
+                return main::_formated($recall);
+            }
+            elsif ($args{flag} eq 'precision') {
+                my $precision = $intersection/$predicted_data_count;
+                return main::_formated($precision);
+            }
+            elsif ($args{flag} eq 'all') {
+                my $recall    = main::_formated($intersection/$ans_data_count);
+                my $precision = main::_formated($intersection/$predicted_data_count);
+                my $res = "$recall\t$precision";
+                return $res;
+            } else {
+                croak "Error: given argument is not acceptable";}
         } else {
-            croak "Error: given argument is not acceptable";}
-    } else {
-        croak "Error: given data is containing zero";
+            croak "Error: given data is containing zero";
+        }
     }
 }
-
-
-sub _formated {
-    my $p = shift;
-    return sprintf "%2.4f", $p;
+    
+package main {
+    use warnings;
+    use strict;
+    
+    sub _formated {
+        my $p = shift;
+        return sprintf "%2.4f", $p;
+    }
 }
 
 1;
-
