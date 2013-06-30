ba52fc6b27060f2d38754b88ba077caeb2ba9d71
soh-i <soh.ishiguro3914@gmail.com>
2013-06-30 16:00:40 +0900

    added AG_richness.pl wrapping sh

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+DIR=/home/soh.i/benchmark
+perl $DIR/src/AGrichness.pl $DIR/data/human/predict/23291724/nmeth.2330-S2_PooledSamplesNon-Repetitive.csv $DIR/nmeth.2330-S3_PooledSamplesNonRepetitive.csv
