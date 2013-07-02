46e11006adb36d8bf811ca2c705ddc97dca108d6
soh-i <soh.ishiguro3914@gmail.com>
2013-07-02 13:24:00 +0900

    Add POD document

--- xls2csv.pl
+++ xls2csv.pl
@@ -2,13 +2,28 @@
 
 use strict;
 use warnings;
+use Getopt::Long;
+use Pod::Usage;
 use Spreadsheet::ParseExcel;
 
+my %args = ();
+my $help = undef;
+
+GetOptions(
+           \%args,
+           'excel=s',
+           'sheet=s',
+           'man|help' => \$help,
+          ) or die pod2Usage(1);
+if ($help) {
+    pod2usage(1);
+}
+
 if (scalar @ARGV != 2) {
-    print STDERR "This script is used for converting xls (excel) to csv.\n";
-    print STDERR "Usage:\n";
-    print "       perl $0 <excel.xls> <SheetName>\n";
-    exit;
+    pod2usage(-verbose   => 2,
+              exitstatus => 0,
+              output     => \*STDERR
+             );
 }
 
 my $input      = shift;
@@ -20,11 +35,6 @@ my $workbook = $parser->parse($input);
 if (!defined $workbook) {
     die $parser->error();
 }
-
-#my $sheet_name = 'PooledSamplesAlu';
-#my $sheet_name = 'PooledSamplesRepetitiveNonAlu';
-#my $sheet_name = 'PooledSamplesNon-Repetitive';
-
 
 for my $worksheet ($workbook->worksheet($sheet_name)) {
     my ($row_min, $row_max) = $worksheet->row_range();
@@ -39,3 +49,25 @@ for my $worksheet ($workbook->worksheet($sheet_name)) {
     }
 }
 
+
+__END__
+
+=head1 NAME
+
+xls2csv - Converting XLS file to CSV
+
+=head1 SYNOPSIS
+
+perl xls2csv --excel data.xls --sheet Sheet1
+
+=head1 OPTIONS
+
+ -e,  --excel     Given a Excel 2003 file.     [Required]
+ -s,  --sheet     Given a sheet name of Excel. [Required]
+ -h,  --help      Show help messages.
+
+=head1 DESCRIPTION
+
+This program converts .xls file to csv.
+
+=cut
