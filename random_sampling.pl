e0b15d27f6b8fb994dc4d694679169451e8a2885
soh-i <soh.ishiguro3914@gmail.com>
2013-07-05 03:45:34 +0900

    Fixed

--- random_sampling.pl
+++ random_sampling.pl
@@ -2,6 +2,25 @@
 
 use strict;
 use warnings;
-use List::Util qw/shuffle/;
+use Data::Dumper;
+use Math::Random::MT;
+use IO::File;
+
+my $file = shift or die;
+
+my $fh = IO::File->new($file) or die;
+my $selected = [];
+my $count = 0;
+while (my $line = $fh->getline()) {
+    chomp $line;
+    my ($chr, $pos) = (split /\,/, $line)[0,1];
+    push $selected, "$chr,$pos,";
+    $count++;
+}
+
+for (my $i=0; $i<1000; $i++) {
+    print $selected->[ int(Math::Random::MT::rand($count)) ], "\n";
+}
+
 
 
