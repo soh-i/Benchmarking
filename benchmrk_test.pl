e589026924a18484feec2a73523e63b4a07bf9a1
soh-i <soh.ishiguro3914@gmail.com>
2013-07-04 20:53:41 +0900

    Changed a suffix of the answer set

--- benchmrk_test.pl
+++ benchmrk_test.pl
@@ -36,7 +36,7 @@ if (!$opts{answer_dir} || !$opts{predicted_dir}) {
 }
 
 # Constatnt variables
-Readonly my $ANSDATA_SUFFIX        => '.txt'; # eg. hg19.txt
+Readonly my $ANSDATA_SUFFIX        => '.csv'; # eg. hg19.csv
 Readonly my $PREDICTED_DATA_SUFFIX => '.csv'; # eg. excel.csv
 
 # Collect answer data set from given dir
@@ -50,14 +50,15 @@ while (my $ans_file = $adh->read()){
 $adh->close();
 
 # Make a header line
-my $header = "Recall\tPrecision\tLabel\tAnswerSet\tDataCount\n";
+my $header = "Recall\tPrecision\tLabel\tAnswerSet\tAnsCount\tPredCount\n";
 print STDOUT $header;
 
 # Roop num. of ans files * num. of predicted files
 for my $ansfile (@ans_files) {
     
     # collection of answer data
-    my $collected_answer = collect_data(file => $ansfile, sep => 'tab');
+    my $collected_answer = collect_data(file => $ansfile, sep => 'comma');
+    
     my $pdh = IO::Dir->new($opts{predicted_dir}) or die $!;
     
     # collection of predicted data
@@ -75,6 +76,7 @@ for my $ansfile (@ans_files) {
         print $result, "\t";
         print basename($pred_file, '.csv'), "\t";
         print basename($ansfile, '.txt'), "\t";
+        print scalar @{$collected_answer}, "\t";
         print scalar @{$collected_predict}, "\n";
     }
     $pdh->close();
@@ -87,9 +89,10 @@ sub collect_data {
                 @_,
                );
     
-    my $bfh = IO::File->new($args{file}) or croak "Cant open file:$!";
+    my $bfh = IO::File->new($args{file}) or croak "Internal Error: Can not open file:$!";
     my $collected = [];
     while (my $data_entory = $bfh->getline) {
+        
         # Skip the header if its exists
         next if __LINE__ == 1;
         next if $data_entory =~ m/^#/;
@@ -97,19 +100,19 @@ sub collect_data {
 
         # Split properly
         if ($args{sep} eq 'tab') {
-            my ($chr, $pos)  = (split /\t/, $data_entory)[0, 1];
+            my ($chr, $pos)  = (split /\t+/, $data_entory)[0,1];
             $chr =~ s/^chr// if $chr =~ m/^chr/; # Remove 'chr' prefix
             $pos =~ s/\,//g  if $pos =~ m/\,/;   # Remove comma. e.g. 123,456
             push $collected, "$chr:$pos";
         }
         elsif ($args{sep} eq 'comma') {
             my ($chr, $pos)  = (split /\,/, $data_entory)[0,1];
-            $chr =~ s/^chr// if $chr =~ m/^chr/; # Remove 'chr' prefix
-            $pos =~ s/\,//g  if $pos =~ m/\,/;   # Remove comma. e.g. 123,456
+            $chr =~ s/^chr// if $chr =~ m/^chr/;
+            $pos =~ s/\,//g  if $pos =~ m/\,/;  
             push $collected, "$chr:$pos";
         }
         else {
-            croak "Error: given split() separetor, 'tab' or 'comma' is only accepted";
+            croak "Internal error: given split() separetor, 'tab' or 'comma' is only accepted";
         }
     }
     return $collected;
